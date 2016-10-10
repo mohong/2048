9aa5fc5271d93eaa674486d9f3f6f5a4fe297a3e
mohong <arthas_qin@qq.com>
2016-10-10 18:24:12 +0800

    初始化完成

---
+++ js/main.js
@@ -0,0 +1,112 @@
+var board = new Array();    //每个格子的数字
+var score = 0;  //分数
+var has_conflicted = new Array();   //解决连续消除的标记
+var success_string = 'Success';
+var gameover_string = 'GameOver';
+
+//初始化棋局
+$(document).ready(function() {
+    new_game();
+});
+
+//开始新游戏
+function new_game() {
+    //初始化棋盘
+    init();
+    //在随机两个格子生成数字
+    generate_one_number();
+    generate_one_number();
+}
+
+//初始化
+function init() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var grid_cell = $('#grid_cell_' + i + '_' + j);
+            grid_cell.css('top', get_pos_top(i, j));
+            grid_cell.css('left', get_pos_left(i, j));
+        }
+    }
+    for (var i = 0; i < 4; i++) {
+        board[i] = new Array();
+        has_conflicted[i] = new Array();
+        for (var j =0; j < 4; j++) {
+            board[i][j] = 0;
+            has_conflicted[i][j] = false;
+        }
+    }
+    update_board_view();
+    score = 0;
+    update_score(score);
+}
+
+//更新棋局
+function update_board_view() {
+    $('.number_cell').remove();
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            $('#grid_container').append('<div class="number_cell" id="number_cell_' + i + '_' + j + '"></div>');
+            var number_cell = $('#number_cell_' + i + '_' + j);
+            if (board[i][j] == 0) {
+                number_cell.css({
+                    'width': '0px',
+                    'height': '0px',
+                    'top': get_pos_top(i, j) +50,
+                    'left': get_pos_left(i, j) +50
+                });
+
+            } else {
+                number_cell.css({
+                    'width':100,
+                    'height':100,
+                    'top': get_pos_top(i, j),
+                    'left': get_pos_left(i, j),
+                    'background-color': get_number_background_color(board[i][j]),
+                    'color': get_number_color(board[i][j]),
+                    'text': board[i][j]
+                });
+            }
+            has_conflicted[i][j] = false;
+        }
+    }
+    $('.number_cell').css({
+        'line-height': '100px',
+        'font-size': '60px'
+    });
+}
+
+//随机在一个格子生成数字
+function generate_one_number() {
+    if (nospace(board)) {
+        return false;
+    }
+    //随机一个位置
+    var randx = parseInt(Math.floor(Math.random() * 4));
+    var randy = parseInt(Math.floor(Math.random() * 4));
+    var time = 0;
+    while (time < 50) {
+        if (board[randx][randy] == 0) {
+            break;
+        }
+        randx = parseInt(Math.floor(Math.random() * 4));
+        randy = parseInt(Math.floor(Math.random() * 4));
+        time++;
+    }
+    if (time == 50) {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 4; j++) {
+                if (board[i][j] == 0) {
+                    randx = i;
+                    randy = j;
+                }
+            }
+        }
+    }
+    //随机一个数字
+    var rand_number = Math.random() < 0.5 ? 2 : 4;
+    //在随机位置显示随机数字
+    board[randx][randy] = rand_number;
+    show_number_with_animation(randx, randy, rand_number);
+    return true;
+}
+
---
+++ js/support.js
@@ -0,0 +1,49 @@
+
+//获得相应格子距离期盘顶部的距离
+function get_pos_top(i, j) {
+    return 20 + i * (20 +100);
+}
+
+//获得相应格子距离棋盘左边的距离
+function get_pos_left(i, j) {
+    return 20 + j * (20 +100);
+}
+
+//获得相应数字的背景色
+function get_number_background_color(number) {
+    switch (number) {
+        case 2: return '#eee4da'; break;
+        case 4: return '#ede0c8'; break;
+        case 8: return '#f2b179'; break;
+        case 16: return '#f59563'; break;
+        case 32: return '#f67c5f'; break;
+        case 64: return '#f65e3b'; break;
+        case 128: return '#edcf72'; break;
+        case 256: return '#edcc61'; break;
+        case 512: return '#9c0'; break;
+        case 1024: return '#33b5e5'; break;
+        case 2048: return '#09c'; break;
+        case 4096: return '#a6c'; break;
+        case 8192: return '#93c'; break;
+    }
+    return 'black';
+}
+
+//获得相应数字的颜色
+function get_number_color(number) {
+    if (number <= 4)
+        return '#776e65';
+    return 'white';
+}
+
+//判断棋盘上是否还有空格子
+function nospace(board) {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
